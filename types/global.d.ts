4a41d710a81433b43d6d648f6cad0a1108a13b41
marsczen <pealstyle@gmail.com>
2023-05-02 23:58:27 +0800

    chore: config update

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,12 +1,21 @@
+declare module '*.jpg'
+declare module '*.png'
+declare module '*.woff2'
+declare module '*.woff'
+declare module '*.ttf'
+declare module '*.scss' {
+  const content: Record<string, string>
+  export default content
+}
+
+declare module '*.svg'
 declare global {
-    /** 使用此类型替代 any object */
-    interface AnyObj {
-      [key: string]: any
-    }
-    /** window 下的全局变量 */
-    interface Window {}
-  
-    interface Storage {}
-  
+  /** 使用此类型替代 any object */
+  interface AnyObj {
+    [key: string]: any
   }
-  
+  /** window 下的全局变量 */
+  interface Window {}
+
+  interface Storage {}
+}
