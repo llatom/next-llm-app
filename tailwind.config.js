96673e087e26e854056a10e9a165f07f77d37c28
marsczen <pealstyle@gmail.com>
2023-05-03 19:03:54 +0800

    feat: tailwind config update and and use-fetch

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {
+      visibility: ['group-hover'],
+    },
+  },
   plugins: [],
-}
+};
